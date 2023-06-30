f92552f990e67888aa88667d7b08e2e0680696ea
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-30 12:45:10 +0300

    2 task is in progress

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,14 @@
 import Player from '@vimeo/player';
-console.log(Player)
+console.log(Player);
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+const player = new Player('handstick', {
+    id: 'vimeo-player',
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+
