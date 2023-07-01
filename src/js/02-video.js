a3e478a8ad20bff3aa58a2063e8155e4f38a4524
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-01 20:02:43 +0300

    Task 2 has been finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,27 @@ import Player from '@vimeo/player';
 console.log(Player);
 const iframe = document.querySelector('iframe');
 console.log(iframe);
-const player = new Player('handstick', {
-    id: 'vimeo-player',
-    width: 640
+const player = new Player(iframe);
+const currentTime = localStorage.getItem('player_current_time');
+player.on('timeupdate', onplay);
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+function onplay({seconds}) {
+    console.log(seconds);
+    localStorage.setItem('player_current_time', JSON.stringify(seconds));
+}
+
 
 
