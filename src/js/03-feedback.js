2a41c1287f8baa7681b8aaf621a9d26dc4a76a0e
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-07-01 23:43:33 +0300

    fixed task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,33 @@
+import throttle from 'lodash.throttle';
+console.log(throttle);
+
 const form = document.querySelector('.feedback-form');
 console.log(form);
+const STORAGE_KEY = "feedback-form-state";
 
-form.addEventListener('input', onInput);
 
-function onInput(e) {
-   const data = { }  ;
-   data[e.target.name] = e.target.value;
-    message = form.elements.message.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(e.target.value))
+const currentData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+form.elements.email.value = currentData.email;
+form.elements.message.value = currentData.message;
+
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+    const formData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    }
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    
+       
+    localStorage.removeItem(STORAGE_KEY);
+        form.reset(); 
+    console.log(currentData);
+   
+    
