1f900a5f5c459ee81a3eec514b75e1eb2b109e95
NataDent <125708241+NataDent@users.noreply.github.com>
2023-07-01 20:44:13 +0000

    Deploying to gh-pages from @ NataDent/goit-js-hw-08@2a41c1287f8baa7681b8aaf621a9d26dc4a76a0e 🚀

--- 03-feedback.c6b32885.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("input",(function(t){({})[t.target.name]=t.target.value,message=e.elements.message.value,localStorage.setItem("feedback-form-state",JSON.stringify(t.target.value))}))}();
-//# sourceMappingURL=03-feedback.c6b32885.js.map
